c3d6007238b90f7e071551d9c421ce2289cee9c7
heter13 <hespinoza@ucsd.edu>
2022-02-10 10:01:11 -0800

    update

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -96,3 +96,31 @@ public class MarkdownParseTest {
 
     
 }
+/*
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.*;
+
+public class MarkdownParseTest {
+    // Setting up a new test case
+    @Test
+    public void addition() {
+        // Asserting that the left of the comma is equal to the right
+        assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void test1() throws IOException{
+        Path fileName = Path.of("test-file.md");
+        System.out.println(fileName.toString());
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);   
+        List<String> expected = List.of(" ");
+        assertEquals(expected, links);
+    }
+}
+*/
